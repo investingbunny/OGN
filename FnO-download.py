850372430d392191afdce99cad41546a1fa45358
HRTR <trharish77@gmail.com>
2020-10-04 12:16:00 +0530

    Fixed csv issue. Need to decide on how to store formatted futures.

--- FnO-download.py
+++ FnO-download.py
@@ -6,20 +6,38 @@ The file is a consolidated daily report which has to be dissected.
 @author: User
 """
 import requests
-from zipfile import ZipFile 
+from zipfile import ZipFile
+import pandas as pd
+from datetime import date
+from datetime import datetime
+import io
+import os
+import pyarrow
+import pyarrow.feather as feather
+from functools import reduce
 
 FnOStartDate = date(2020,9,1)
 FnOReport = 'https://archives.nseindia.com/archives/fo/mkt/'
 FnOVolatility = 'https://archives.nseindia.com/archives/nsccl/volt/'
+# FnOBhavCopy = 'https://archives.nseindia.com/content/historical/DERIVATIVES/'#2020/OCT/fo01OCT2020bhav.csv.zip
 
 # FnOReportArg #fo30092020.zip
 # FnOVolatilityArg #FOVOLT_29092020.csv
+HolidayList = ['21-Feb-20','10-Mar-20','2-Apr-20','6-Apr-20','10-Apr-20','14-Apr-20','1-May-20','25-May-20','2-Oct-20','16-Nov-20','30-Nov-20','25-Dec-20']
+HolidayList = pd.to_datetime(pd.Series(HolidayList), format='%d-%b-%y')
+bday = pd.bdate_range("2020-09-01", "2020-10-04") #To be replaced with LastRecordDate, CurrentDate
+bday = set(bday).difference(HolidayList)
 
-def DownloadNewNSEdata():
-    bday = pd.bdate_range("2020-09-01", "2020-10-03") #To be replaced with LastRecordDate, CurrentDate
+def FindFeather(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+def DownloadNewNSEFnO():
+
     #Loop through dates to download NSE data
     for weekday in bday:
-        # print(weekday.strftime("%d%m%Y"))
+        print(weekday.strftime("%d%m%Y"))
         FnOReportArg = 'fo' + weekday.strftime("%d%m%Y") + '.zip'
         FnOReportURL = FnOReport + FnOReportArg
         try:
@@ -27,26 +45,62 @@ def DownloadNewNSEdata():
             open(FnOReportArg, 'wb').write(r.content)
         except:
             print('Couldnt download:'+ FnOReportURL)
-        
+
+def DownloadNewNSEVolatility():        
+    #Loop through dates to download NSE data
+    for weekday in bday:
         FnOVolatilityArg = 'FOVOLT_' + weekday.strftime("%d%m%Y") + '.csv'
-        FnOVolatilityURL = FnOReport + FnOReportArg
+        FnOVolatilityURL = FnOVolatility + FnOVolatilityArg
         try:
             r = requests.get(FnOVolatilityURL, allow_redirects=True) #Download FnO Volatility report for 'weekday'
-            open(FnOVolatilityArg, 'wb').write(r.content)
+            if r.ok:
+                data = r.content.decode('utf8')
+                Voldf = pd.read_csv(io.StringIO(data))
+                Voldf = Voldf.rename(columns=lambda x: x.strip())
         except:
             print('Couldnt download:'+ FnOVolatilityURL)    
-
+            
+        if not Voldf.empty:
+            feather.write_feather(Voldf, './New NSE site/'+FnOVolatilityArg+'.ftr')
+            
 def ExtractNSEFnOData():
     # bday = pd.bdate_range("2020-09-01", "2020-10-03") #To be replaced with LastRecordDate, CurrentDate
     # FnOReportArg = 'fo' + weekday.strftime("%d%m%Y") + '.zip'
     FnOReportArg = 'fo01102020'
+    FnOVolatilityArg = 'FOVOLT_01102020.csv.ftr'
 
     zf = ZipFile('New NSE site/'+FnOReportArg+'.zip') 
     CSVdf = pd.read_csv(zf.open(FnOReportArg+'.csv'), parse_dates=[2], dayfirst=True)
     CSVdf = CSVdf.rename(columns=lambda x: x.strip())
     NewFuturesdf = RefineNewNSEFutures(CSVdf)
 
-
+    ExpiryDatedf = NewFuturesdf.groupby('Symbol')['Expiry'].apply(lambda x: pd.Series(list(x))).unstack() 
+    ExpiryDatedf = ExpiryDatedf.reset_index(level=0)
+    ExpiryDatedf = ExpiryDatedf.rename(columns={0: 'NearExpiry',1:'MidExpiry',2:'FarExpiry'})
+    
+    SettlePricedf = NewFuturesdf.groupby('Symbol')['Settle Price'].apply(lambda x: pd.Series(list(x))).unstack()
+    SettlePricedf = SettlePricedf.reset_index(level=0)
+    SettlePricedf = SettlePricedf.rename(columns={0: 'NearSettlePrice',1:'MidSettlePrice',2:'FarSettlePrice'})
+  
+    OpenInterestdf = NewFuturesdf.groupby('Symbol')['Open Interest'].apply(lambda x: pd.Series(list(x))).unstack()
+    OpenInterestdf = OpenInterestdf.reset_index(level=0)
+    OpenInterestdf = OpenInterestdf.rename(columns={0: 'NearOpenInterest',1:'MidOpenInterest',2:'FarOpenInterest'})        
+    
+    NoOfContractsdf = NewFuturesdf.groupby('Symbol')['Number of Contracts'].apply(lambda x: pd.Series(list(x))).unstack()
+    NoOfContractsdf = NoOfContractsdf.reset_index(level=0)
+    NoOfContractsdf = NoOfContractsdf.rename(columns={0: 'NearNoOfContracts',1:'MidNoOfContracts',2:'FarNoOfContracts'})        
+      
+    dfs = [ExpiryDatedf, OpenInterestdf, SettlePricedf, NoOfContractsdf]
+    Futdf = reduce(lambda left,right: pd.merge(left,right,on='Symbol'), dfs)   
+    
+    if (FindFeather(FnOVolatilityArg, './New NSE site/')):
+        Volatilitydf = feather.read_feather('./New NSE site/'+FnOVolatilityArg) #Volatilitydf.info()
+        #Below Formatting is needed to clean up the csv and remove whitespaces
+        Volatilitydf = Volatilitydf.rename(columns=lambda x: x.strip())
+        Futdf = Futdf.rename(columns=lambda x: x.strip())
+        Futdf = Futdf.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        Volatilitydf = pd.merge(Futdf, Volatilitydf, how="outer", on=["Symbol"])
+    
 def RefineNewNSEFutures(DF):
     # df.info()
     df = DF.copy()
