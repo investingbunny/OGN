e9766934ea275fef4e17d38e0bc4e3ef5a683c56
HRTR <trharish77@gmail.com>
2020-10-16 10:10:49 +0530

    Migration of Futures and Options data update to new site complete

--- FnO-download.py
+++ FnO-download.py
@@ -35,7 +35,7 @@ def FindFeather(name, path):
             return os.path.join(root, name)
 
 def UpdateBusinessDays():
-    #Check Index futures for last update
+    #Check Index futures for last update NiftyFullFutures.info()
     NiftyFullFutures = feather.read_feather('./Datastore/NIFTY_full-futures.ftr')
     FuturesStartDate = NiftyFullFutures.iloc[-1].Date
     FuturesStartDate += datetime.timedelta(days=1)
@@ -87,69 +87,10 @@ def DownloadNewNSEFnO():
             
         if not Voldf.empty:
             feather.write_feather(Voldf, './New NSE site/'+FnOVolatilityArg+'.ftr')
-            
-        #FnO Settlement Report download    
-        FnOSettleArg = 'FOSett_prce_' + weekday.strftime("%d%m%Y") + '.csv'
-        FnOSettlementURL = FnOSettlement + FnOSettleArg
-        try:
-            r = requests.get(FnOSettlementURL, allow_redirects=True) #Download FnO Volatility report for 'weekday'
-            if r.ok:
-                data = r.content.decode('utf8')
-                Setdf = pd.read_csv(io.StringIO(data))
-                Setdf = Setdf.rename(columns=lambda x: x.strip())
-                Setdf = Setdf.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-                Setdf = RefineNewNSEFutures(Setdf)
-        except:
-            print('Couldnt download:'+ FnOSettlementURL)    
-            
-        if not Setdf.empty:
-            feather.write_feather(Setdf, './New NSE site/'+FnOSettleArg+'.ftr')
 
-def UpdatetNSEFuturesData():
-    TotalNewFuturesdf = pd.DataFrame()
-    for weekday in bday:
-        FnOReportArg = 'fo' + weekday.strftime("%d%m%Y")
-        FnOSettleArg = 'FOSett_prce_' + weekday.strftime("%d%m%Y") + '.csv.ftr'
-    
-        zf = ZipFile('New NSE site/'+FnOReportArg + '.zip') 
-        CSVdf = pd.read_csv(zf.open(FnOReportArg+'.csv'), parse_dates=[2], dayfirst=True)
-        CSVdf = CSVdf.rename(columns=lambda x: x.strip())
-        CSVdf = CSVdf.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-        NewFuturesdf = RefineNewNSEFutures(CSVdf)
-        
-        if (FindFeather(FnOSettleArg, './New NSE site/')):
-            Settledf = feather.read_feather('./New NSE site/'+FnOSettleArg) #Volatilitydf.info()
-            NewFuturesdf = pd.merge(NewFuturesdf, Settledf, how="inner", on=["Symbol","Expiry","Instrument"])
-        else:
-            print('Couldnt find settlement info for '+ FnOSettleArg)
-        
-        TotalNewFuturesdf = TotalNewFuturesdf.append(NewFuturesdf, ignore_index=True)
-    
-    TotalNewFuturesdf = TotalNewFuturesdf.sort_values(by=['Symbol', 'Date'])
-    FnOSymbollist = []
-    #Adding values to list
-    FnOSymbollist = list(TotalNewFuturesdf['Symbol'])
-    #Removing duplicates in list
-    FnOSymbollist = list(dict.fromkeys(FnOSymbollist))
-    
-    #Update the old Futures file
-    for sym in FnOSymbollist:
-        
-        FuturesFileName = sym + '_' + FullFuturesFilePath
-        #Read from feather
-        if (FindFeather(FuturesFileName, './Datastore/')):
-            OldFuturesdf = feather.read_feather('./Datastore/'+FuturesFileName)
-            print('Updating Futures for '+ sym)
-            Mergedf = OldFuturesdf.append(TotalNewFuturesdf[TotalNewFuturesdf["Symbol"] == sym], ignore_index = True)            
-        else: #A new symbol has been added, create a feather for it
-            print('Creating new Futures DB for '+ sym)
-            Mergedf = TotalNewFuturesdf[TotalNewFuturesdf["Symbol"] == sym]#, ignore_index = True)
-            
-        if not Mergedf.empty:
-            feather.write_feather(Mergedf, './Datastore/'+FuturesFileName)
-            
-def UpdatetNSEOptionsData():
+def UpdatetNSEFnOData():
     TotalNewOptionsdf = pd.DataFrame()
+    TotalNewFuturesdf = pd.DataFrame()
     for weekday in bday:  # weekday = YesterdayDate
         FnOBhavArg = 'fo' + weekday.strftime("%d%b%Y").upper() + 'bhav'
         print('Processing '+FnOBhavArg)
@@ -157,34 +98,53 @@ def UpdatetNSEOptionsData():
         CSVdf = pd.read_csv(zf.open(FnOBhavArg+'.csv'), parse_dates=[2], dayfirst=True) #fo12OCT2020bhav
         CSVdf = CSVdf.rename(columns=lambda x: x.strip())
         CSVdf = CSVdf.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-        NewOptionsdf = RefineNewNSEOptions(CSVdf)
-        NewOptionsdf = NewOptionsdf[(NewOptionsdf["Instrument"] == 'OPTIDX') | (NewOptionsdf["Instrument"] == 'OPTSTK')]
+        NewFnOdf = RefineNSEFnOData(CSVdf)
+        NewFuturesdf = NewFnOdf[(NewFnOdf["Instrument"] == 'FUTIDX') | (NewFnOdf["Instrument"] == 'FUTSTK')]
+        NewOptionsdf = NewFnOdf[(NewFnOdf["Instrument"] == 'OPTIDX') | (NewFnOdf["Instrument"] == 'OPTSTK')]
         
         TotalNewOptionsdf = TotalNewOptionsdf.append(NewOptionsdf, ignore_index=True)
+        TotalNewFuturesdf = TotalNewFuturesdf.append(NewFuturesdf, ignore_index=True)
     
     TotalNewOptionsdf = TotalNewOptionsdf.sort_values(by=['Symbol', 'Date'])
+    TotalNewFuturesdf = TotalNewFuturesdf.sort_values(by=['Symbol', 'Date'])
     FnOSymbollist = []
     #Adding values to list
     FnOSymbollist = list(TotalNewOptionsdf['Symbol'])
     #Removing duplicates in list
     FnOSymbollist = list(dict.fromkeys(FnOSymbollist))
     
-    #Update the old Options file
-    for sym in FnOSymbollist:
+    for sym in FnOSymbollist: #sym = 'NIFTY'
+        #Update the old Options file
         OptionsFileName = sym + '_' + MonthlyOptionsFilePath
         #Read from feather
         if (FindFeather(OptionsFileName, './Datastore/')):
             OldOptionsdf = feather.read_feather('./Datastore/'+OptionsFileName)
             print('Updating Options for '+ sym)
-            Mergedf = OldOptionsdf.append(TotalNewOptionsdf[TotalNewOptionsdf["Symbol"] == sym], ignore_index = True)            
+            MergeOptdf = OldOptionsdf.append(TotalNewOptionsdf[TotalNewOptionsdf["Symbol"] == sym], ignore_index = True)            
         else: #A new symbol has been added, create a feather for it
             print('Creating new Options DB for '+ sym)
-            Mergedf = TotalNewOptionsdf[TotalNewOptionsdf["Symbol"] == sym]
+            MergeOptdf = TotalNewOptionsdf[TotalNewOptionsdf["Symbol"] == sym]
             
-        if not Mergedf.empty:
-            feather.write_feather(Mergedf, './Datastore/'+OptionsFileName)
+        if not MergeOptdf.empty:
+            feather.write_feather(MergeOptdf, './Datastore/'+OptionsFileName)
+        
+        #Update the old Futures file
+        FuturesFileName = sym + '_' + FullFuturesFilePath
+        #Read from feather
+        if (FindFeather(FuturesFileName, './Datastore/')):
+            OldFuturesdf = feather.read_feather('./Datastore/'+FuturesFileName)
+            print('Updating Futures for '+ sym)
+            MergeFutdf = OldFuturesdf.append(TotalNewFuturesdf[TotalNewFuturesdf["Symbol"] == sym], ignore_index = True)            
+        else: #A new symbol has been added, create a feather for it
+            print('Creating new Futures DB for '+ sym)
+            MergeFutdf = TotalNewFuturesdf[TotalNewFuturesdf["Symbol"] == sym]
+            
+        if not MergeFutdf.empty:
+            MergeFutdf.drop("Strike Price", axis=1, inplace=True)
+            MergeFutdf.drop("Option type", axis=1, inplace=True)
+            feather.write_feather(MergeFutdf, './Datastore/'+FuturesFileName)
 
-def RefineNewNSEOptions(DF):
+def RefineNSEFnOData(DF):
     df = DF.copy()
     
     if 'INSTRUMENT' in df.columns:
@@ -241,70 +201,7 @@ def RefineNewNSEOptions(DF):
         
     return df
 
-def RefineNewNSEFutures(DF):
-    df = DF.copy()
-    
-    if 'INSTRUMENT' in df.columns:
-        df.rename(columns={'INSTRUMENT': 'Instrument'}, inplace=True)
-        
-    if 'SYMBOL' in df.columns:
-        df.rename(columns={'SYMBOL': 'Symbol'}, inplace=True)
-    
-    if 'EXP_DATE' in df.columns:
-        df.rename(columns={'EXP_DATE': 'Expiry'}, inplace=True)
-        # df["Expiry"] = df["EXP_DATE"].apply(pd.to_datetime, format='%d-%b-%Y')
-        df['Expiry'] = df['Expiry'].dt.date
-     
-    if 'OPEN_PRICE' in df.columns:
-        df.rename(columns={'OPEN_PRICE': 'Open'}, inplace=True) 
-        
-    if 'HI_PRICE' in df.columns:
-        df.rename(columns={'HI_PRICE': 'High'}, inplace=True)
-        
-    if 'LO_PRICE' in df.columns:
-        df.rename(columns={'LO_PRICE': 'Low'}, inplace=True)
-    
-    if 'CLOSE_PRICE' in df.columns:
-        df.rename(columns={'CLOSE_PRICE': 'Close'}, inplace=True)
-
-    if 'OPEN_INT*' in df.columns:
-        df.rename(columns={'OPEN_INT*': 'Open Interest'}, inplace=True)
-
-    if 'TRD_VAL' in df.columns:
-        df.rename(columns={'TRD_VAL': 'Turnover'}, inplace=True)                  
-        
-    if 'NO_OF_CONT' in df.columns:
-        df.rename(columns={'NO_OF_CONT': 'Number of Contracts'}, inplace=True)      
-
-    if 'DATE' in df.columns:
-        df.rename(columns={'DATE': 'Date'}, inplace=True)
-        df['Date'] = df['Date'].apply(pd.to_datetime, format='%d-%b-%Y')
-        df['Date'] = df['Date'].dt.date
-    
-    if 'UNDERLYING' in df.columns:
-        df.rename(columns={'UNDERLYING': 'Symbol'}, inplace=True)
-
-    if 'INSTRUMENT' in df.columns:
-        df.rename(columns={'INSTRUMENT': 'Instrument'}, inplace=True)
-
-    if 'EXPIRY DATE' in df.columns:
-        df.rename(columns={'EXPIRY DATE': 'Expiry'}, inplace=True)
-        df['Expiry'] = df['Expiry'].apply(pd.to_datetime, format='%d-%b-%Y')
-        df['Expiry'] = df['Expiry'].dt.date
-
-    if 'MTM SETTLEMENT PRICE' in df.columns:
-        df.rename(columns={'MTM SETTLEMENT PRICE': 'Settle Price'}, inplace=True)
-        
-    return df
-
 def main():
     UpdateBusinessDays()
     DownloadNewNSEFnO()
-    UpdatetNSEFuturesData()
-    UpdatetNSEOptionsData()
-    
-    
-            # ###########OldFuturesdf.drop("index", axis=1, inplace=True) #DO NOT ENABLE!!!
-            # ############OldFuturesdf = OldFuturesdf[OldFuturesdf.Date < FnOStartDate] #DO NOT ENABLE!!!
-            # ########OldOptionsdf = OldOptionsdf[OldOptionsdf.Date < FnOStartDate] #DO NOT ENABLE!!!            
-            
+    UpdatetNSEFnOData()
--- temp.py
+++ temp.py
@@ -4,6 +4,151 @@ Created on Tue Aug 18 11:54:38 2020
 
 @author: User
 """
+
+            
+        # #FnO Settlement Report download    
+        # FnOSettleArg = 'FOSett_prce_' + weekday.strftime("%d%m%Y") + '.csv'
+        # FnOSettlementURL = FnOSettlement + FnOSettleArg
+        # try:
+        #     r = requests.get(FnOSettlementURL, allow_redirects=True) #Download FnO Volatility report for 'weekday'
+        #     if r.ok:
+        #         data = r.content.decode('utf8')
+        #         Setdf = pd.read_csv(io.StringIO(data))
+        #         Setdf = Setdf.rename(columns=lambda x: x.strip())
+        #         Setdf = Setdf.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        #         Setdf = RefineNewNSEFutures(Setdf)
+        # except:
+        #     print('Couldnt download:'+ FnOSettlementURL)    
+            
+        # if not Setdf.empty:
+        #     feather.write_feather(Setdf, './New NSE site/'+FnOSettleArg+'.ftr')    
+
+
+def RefineNewNSEFutures(DF):
+    df = DF.copy()
+    
+    if 'INSTRUMENT' in df.columns:
+        df.rename(columns={'INSTRUMENT': 'Instrument'}, inplace=True)
+        
+    if 'SYMBOL' in df.columns:
+        df.rename(columns={'SYMBOL': 'Symbol'}, inplace=True)
+    
+    if 'EXP_DATE' in df.columns:
+        df.rename(columns={'EXP_DATE': 'Expiry'}, inplace=True)
+        # df["Expiry"] = df["EXP_DATE"].apply(pd.to_datetime, format='%d-%b-%Y')
+        df['Expiry'] = df['Expiry'].dt.date
+     
+    if 'OPEN_PRICE' in df.columns:
+        df.rename(columns={'OPEN_PRICE': 'Open'}, inplace=True) 
+        
+    if 'HI_PRICE' in df.columns:
+        df.rename(columns={'HI_PRICE': 'High'}, inplace=True)
+        
+    if 'LO_PRICE' in df.columns:
+        df.rename(columns={'LO_PRICE': 'Low'}, inplace=True)
+    
+    if 'CLOSE_PRICE' in df.columns:
+        df.rename(columns={'CLOSE_PRICE': 'Close'}, inplace=True)
+
+    if 'OPEN_INT*' in df.columns:
+        df.rename(columns={'OPEN_INT*': 'Open Interest'}, inplace=True)
+
+    if 'TRD_VAL' in df.columns:
+        df.rename(columns={'TRD_VAL': 'Turnover'}, inplace=True)                  
+        
+    if 'NO_OF_CONT' in df.columns:
+        df.rename(columns={'NO_OF_CONT': 'Number of Contracts'}, inplace=True)      
+
+    if 'DATE' in df.columns:
+        df.rename(columns={'DATE': 'Date'}, inplace=True)
+        df['Date'] = df['Date'].apply(pd.to_datetime, format='%d-%b-%Y')
+        df['Date'] = df['Date'].dt.date
+    
+    if 'UNDERLYING' in df.columns:
+        df.rename(columns={'UNDERLYING': 'Symbol'}, inplace=True)
+
+    if 'INSTRUMENT' in df.columns:
+        df.rename(columns={'INSTRUMENT': 'Instrument'}, inplace=True)
+
+    if 'EXPIRY DATE' in df.columns:
+        df.rename(columns={'EXPIRY DATE': 'Expiry'}, inplace=True)
+        df['Expiry'] = df['Expiry'].apply(pd.to_datetime, format='%d-%b-%Y')
+        df['Expiry'] = df['Expiry'].dt.date
+
+    if 'MTM SETTLEMENT PRICE' in df.columns:
+        df.rename(columns={'MTM SETTLEMENT PRICE': 'Settle Price'}, inplace=True)
+        
+    return df
+
+# for sym in FnOSymbollist:
+#     FuturesFileName = sym + '_' + FullFuturesFilePath
+#     #Read from feather
+#     if (FindFeather(FuturesFileName, './Datastore/')):
+#         OldFuturesdf = feather.read_feather('./Datastore/'+FuturesFileName)
+#         df = OldFuturesdf.copy()
+#         print('Updating Futures for '+ sym)
+#         if 'Number of Contracts' in df.columns:
+#             df.rename(columns={'Number of Contracts': 'No. of contracts'}, inplace=True)
+        
+#         if 'Turnover' in df.columns:
+#             df.rename(columns={'Turnover': 'Turnover in Lacs'}, inplace=True)
+
+#         if 'Open Interest' in df.columns:
+#             df.rename(columns={'Open Interest': 'Open Int'}, inplace=True)
+        
+#     if not df.empty:
+#         feather.write_feather(df, './Datastore/'+FuturesFileName)
+
+    
+            # ###########OldFuturesdf.drop("index", axis=1, inplace=True) #DO NOT ENABLE!!!
+            # ############OldFuturesdf = OldFuturesdf[OldFuturesdf.Date < FnOStartDate] #DO NOT ENABLE!!!
+            # ########OldOptionsdf = OldOptionsdf[OldOptionsdf.Date < FnOStartDate] #DO NOT ENABLE!!!            
+            
+            
+# def UpdatetNSEFuturesData():
+#     TotalNewFuturesdf = pd.DataFrame()
+#     for weekday in bday:
+#         FnOReportArg = 'fo' + weekday.strftime("%d%m%Y")
+#         FnOSettleArg = 'FOSett_prce_' + weekday.strftime("%d%m%Y") + '.csv.ftr'
+    
+#         zf = ZipFile('New NSE site/'+FnOReportArg + '.zip') 
+#         CSVdf = pd.read_csv(zf.open(FnOReportArg+'.csv'), parse_dates=[2], dayfirst=True)
+#         CSVdf = CSVdf.rename(columns=lambda x: x.strip())
+#         CSVdf = CSVdf.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+#         NewFuturesdf = RefineNewNSEFutures(CSVdf)
+        
+#         if (FindFeather(FnOSettleArg, './New NSE site/')):
+#             Settledf = feather.read_feather('./New NSE site/'+FnOSettleArg) #Volatilitydf.info()
+#             NewFuturesdf = pd.merge(NewFuturesdf, Settledf, how="inner", on=["Symbol","Expiry","Instrument"])
+#         else:
+#             print('Couldnt find settlement info for '+ FnOSettleArg)
+        
+#         TotalNewFuturesdf = TotalNewFuturesdf.append(NewFuturesdf, ignore_index=True)
+    
+#     TotalNewFuturesdf = TotalNewFuturesdf.sort_values(by=['Symbol', 'Date'])
+#     FnOSymbollist = []
+#     #Adding values to list
+#     FnOSymbollist = list(TotalNewFuturesdf['Symbol'])
+#     #Removing duplicates in list
+#     FnOSymbollist = list(dict.fromkeys(FnOSymbollist))
+    
+#     #Update the old Futures file
+#     for sym in FnOSymbollist:
+        
+#         FuturesFileName = sym + '_' + FullFuturesFilePath
+#         #Read from feather
+#         if (FindFeather(FuturesFileName, './Datastore/')):
+#             OldFuturesdf = feather.read_feather('./Datastore/'+FuturesFileName)
+#             print('Updating Futures for '+ sym)
+#             Mergedf = OldFuturesdf.append(TotalNewFuturesdf[TotalNewFuturesdf["Symbol"] == sym], ignore_index = True)            
+#         else: #A new symbol has been added, create a feather for it
+#             print('Creating new Futures DB for '+ sym)
+#             Mergedf = TotalNewFuturesdf[TotalNewFuturesdf["Symbol"] == sym]#, ignore_index = True)
+            
+#         if not Mergedf.empty:
+#             feather.write_feather(Mergedf, './Datastore/'+FuturesFileName)
+
+
     ExpiryDatedf = NewFuturesdf.groupby('Symbol')['Expiry'].apply(lambda x: pd.Series(list(x))).unstack() 
     ExpiryDatedf = ExpiryDatedf.reset_index(level=0)
     ExpiryDatedf = ExpiryDatedf.rename(columns={0: 'NearExpiry',1:'MidExpiry',2:'FarExpiry'})
